14e9706e0bbb5e5b0254ba6346d56fdcd5d5ccf6
Shubh Sinha <riotstriker@gmail.com>
2026-06-10 01:29:51 +0530

    RBAC Implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -60,7 +61,90 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required"
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server Error"
+    });
+  }
+};
+
+exports.getProfile = async (
+  req,
+  res
+) => {
+
   res.json({
-    message: "Login endpoint"
+    success: true,
+    user: req.user
   });
+
+};
+
+exports.adminDashboard = async (
+  req,
+  res
+) => {
+
+  res.json({
+    success: true,
+    message:
+      "Welcome Admin Dashboard"
+  });
+
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,45 @@
 const express = require("express");
 
 const router = express.Router();
+const authorize = require(
+  "../middleware/rbac.middleware"
+);
 
 const {
   register,
-  login
+  login,
+  getProfile,
+  adminDashboard
 } = require(
   "../controllers/auth.controller"
 );
 
-router.post("/register", register);
+const protect = require(
+  "../middleware/auth.middleware"
+);
 
-router.post("/login", login);
+router.post(
+  "/register",
+  register
+);
 
+router.post(
+  "/login",
+  login
+);
+
+router.get(
+  "/me",
+  protect,
+  getProfile
+);
+
+
+
+router.get(
+  "/admin",
+  protect,
+  authorize("ADMIN"),
+  adminDashboard
+);
 module.exports = router;
